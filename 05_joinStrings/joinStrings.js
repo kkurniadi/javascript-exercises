1984f73f0080ffa631557a829b0158f1ec69d90d
kkurniadi <90660898+kkurniadi@users.noreply.github.com>
2025-07-21 14:41:04 +0800

    Create joinStrings step 2 variables

--- 05_joinStrings/joinStrings.js
+++ 05_joinStrings/joinStrings.js
@@ -8,11 +8,11 @@
 */
 
 // Add your code right below, good luck!
-
-
-
-
-
+firstName = "Carlos"
+lastName = "Stevenson"
+thisYear = 1965
+birthYear = 1947
+greeting = "Hello! My name is " + firstName + " " + lastName + " and I am " + (thisYear - birthYear) + " years old."
 // Do not change this
 module.exports = {
   firstName: typeof firstName === 'undefined' ? undefined : firstName,
