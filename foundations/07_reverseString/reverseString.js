e40f1c66e3c4daf10de04b6a13e06506f18cb6a8
kkurniadi <90660898+kkurniadi@users.noreply.github.com>
2026-02-08 22:56:13 +0800

    Write reverseString function

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(str) {
+  let reverseChars = [];
 
+  for (let i = str.length - 1; i > 0; i--) {
+    reverseChars.push(str.charAt(i));
+  }
+
+  return reverseChars.join("");
 };
 
 // Do not edit below this line
