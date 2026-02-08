aaef273f492e105df90501fb29fb930f063fe25b
kkurniadi <90660898+kkurniadi@users.noreply.github.com>
2026-02-08 23:44:12 +0800

    Add sumAll function guard clauses

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,6 @@
 const sumAll = function(a, b) {
-
+  if (typeof a !== "number" || typeof b !== "number") return 'ERROR';
+  if (!Number.isInteger(a) || !Number.isInteger(b)) return 'ERROR';
 };
 
 // Do not edit below this line
