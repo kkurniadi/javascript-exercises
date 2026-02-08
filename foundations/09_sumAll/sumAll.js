1f9311a39fe1fbbccdf3aac92abe182230cbf4dc
kkurniadi <90660898+kkurniadi@users.noreply.github.com>
2026-02-09 00:28:02 +0800

    Fix bug in sumAll function

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -2,9 +2,9 @@ const sumAll = function(a, b) {
   if (typeof a !== "number" || typeof b !== "number") return 'ERROR';
   if (!Number.isInteger(a) || !Number.isInteger(b)) return 'ERROR';
 
-  let allInts = [a, b].sort((x, y) => x - y);
-  const min = Math.min(allInts);
-  const max = Math.max(allInts);
+  let allInts = [];
+  const min = Math.min(a, b);
+  const max = Math.max(a, b);
   for (let i = min; i <= max; i++) {
     allInts.push(i);
   }
