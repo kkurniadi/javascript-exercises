67d7e3a2bb6bf4841d0ff51d23655fce673fecb0
kkurniadi <90660898+kkurniadi@users.noreply.github.com>
2026-02-09 00:29:04 +0800

    Enable sumAll test 2

--- foundations/09_sumAll/sumAll.spec.js
+++ foundations/09_sumAll/sumAll.spec.js
@@ -1,10 +1,10 @@
 const sumAll = require('./sumAll')
 
 describe('sumAll', () => {
-  test('sums numbers within the range', () => {
+  test.skip('sums numbers within the range', () => {
     expect(sumAll(2, 4)).toEqual(9);
   });
-  test.skip('works with large numbers', () => {
+  test('works with large numbers', () => {
     expect(sumAll(1, 4000)).toEqual(8002000);
   });
   test.skip('works with larger number first', () => {
