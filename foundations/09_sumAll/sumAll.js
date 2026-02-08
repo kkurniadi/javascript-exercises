f379bdee19baabd7a464e6e05ee4749ffceb68a8
kkurniadi <90660898+kkurniadi@users.noreply.github.com>
2026-02-09 00:13:03 +0800

    Populate allInts array

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,13 @@
 const sumAll = function(a, b) {
   if (typeof a !== "number" || typeof b !== "number") return 'ERROR';
   if (!Number.isInteger(a) || !Number.isInteger(b)) return 'ERROR';
+
+  let allInts = [a, b].sort((x, y) => x - y);
+  const min = Math.min(allInts);
+  const max = Math.max(allInts);
+  for (let i = min + 1; i < max; i++) {
+    allInts.push(i);
+  }
 };
 
 // Do not edit below this line
