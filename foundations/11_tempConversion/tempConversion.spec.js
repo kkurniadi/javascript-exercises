b1eecb09c9db958880a5c02cedb64f675cf45eb0
kkurniadi <90660898+kkurniadi@users.noreply.github.com>
2026-02-09 01:12:31 +0800

    Enable tempConversion test 5

--- foundations/11_tempConversion/tempConversion.spec.js
+++ foundations/11_tempConversion/tempConversion.spec.js
@@ -13,10 +13,10 @@ describe('convertToCelsius', () => {
 });
 
 describe('convertToFahrenheit', () => {
-  test('works', () => {
+  test.skip('works', () => {
     expect(convertToFahrenheit(0)).toEqual(32);
   });
-  test.skip('rounds to 1 decimal', () => {
+  test('rounds to 1 decimal', () => {
     expect(convertToFahrenheit(73.2)).toEqual(163.8);
   });
   test.skip('works with negatives', () => {
